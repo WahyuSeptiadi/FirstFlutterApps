23d54b69f009eacba37f53cc8fb896a6538bf522
WahyuSeptiadi <wahyusptd@gmail.com>
2021-05-12 08:30:03 +0700

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
           title: Text('Hello'),
         ),
         body: Center(
-          child: Heading(text: 'Hello, world!'), // Custom text with Heading
+          // child: Heading(text: 'Hello, world!'), // Custom text with Heading
+          child: BiggerText(text: 'BIGGER'), // Custom text with BiggerText
         ),
       ),
     );
@@ -37,6 +38,38 @@ class Heading extends StatelessWidget { // state yang tidak bisa berubah (immuta
         fontSize: 24.0,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget { // state yang bisa berubah-ubah
+  final String text; // BUKAN STATE ASLI dari StatefulWidget
+
+  const BiggerText({this.text});
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0; // STATE ASLI dari StatefulWidget
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: Text("Zoom In"),
+          onPressed: () {
+            setState(() { // Mengubah nilai state harus dilakukan pada fungsi setState
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
