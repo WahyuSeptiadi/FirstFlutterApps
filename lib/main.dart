df6fca7993a300f90716f179f56593f653d5c8cb
WahyuSeptiadi <wahyusptd@gmail.com>
2021-05-12 08:16:10 +0700

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,25 @@ class MyApp extends StatelessWidget {
           title: Text('Hello'),
         ),
         body: Center(
-          child: Text('Hello, world!'),
+          child: Heading(text: 'Hello, world!'), // Custom text with Heading
         ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget { // state yang tidak bisa berubah (immutable)
+  final String text; // maka dari itu state harus dibuat FINAL
+
+  Heading({this.text}); // dan value di set di constructor nya
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
