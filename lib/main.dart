5919463bfdf2c26127c9ad8027acba6e16bceb5f
WahyuSeptiadi <wahyusptd@gmail.com>
2021-05-15 16:07:43 +0700

    create Scaffold (tampilan dasar flutter)

--- lib/main.dart
+++ lib/main.dart
@@ -12,74 +12,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello'),
-        ),
-        body: Center(
-          // child: Heading(text: 'Hello, world!'), // Custom text with Heading
-          child: BiggerText(text: 'BIGGER'), // Custom text with BiggerText
-        ),
-      ),
+      home: FirstScreen(), // get FirstScreen in here
     );
   }
 }
 
-class Heading extends StatelessWidget {
-  // state yang tidak bisa berubah (immutable)
-  final String text; // maka dari itu state harus dibuat FINAL
-
-  Heading({this.text}); // dan value di set di constructor nya
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  // state yang bisa berubah-ubah
-  final String text; // BUKAN STATE ASLI dari StatefulWidget
-
-  const BiggerText({this.text});
-
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0; // STATE ASLI dari StatefulWidget
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: Text("Zoom In"),
-          onPressed: () {
-            setState(() {
-              // Mengubah nilai state harus dilakukan pada fungsi setState
-              _textSize = 32.0;
-            });
-          },
-        ),
-        ElevatedButton(
-          child: Text("Zoom Out"),
-          onPressed: () {
-            setState(() {
-              _textSize = 16.0;
-            });
-          },
-        )
-      ],
-    );
+    return Scaffold();
   }
 }
