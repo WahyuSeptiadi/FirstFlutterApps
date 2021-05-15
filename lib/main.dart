64879e3c55fae3d3bb2ca7e50bcf84d59a7d8a93
WahyuSeptiadi <wahyusptd@gmail.com>
2021-05-15 16:15:27 +0700

    add icon in AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,21 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton( // icon drawer
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ), onPressed: () {  },
+        ),
         title: Text('First Screen'),
+        actions: <Widget>[ // icon search
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ), onPressed: () {  },
+          ),
+        ],
       ),
     );
   }
